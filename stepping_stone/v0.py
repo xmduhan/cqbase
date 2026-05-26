4c814246e8b00db9da9f132f0ce1473e98459b2c
duhan <duhan@189.cn>
2026-05-26 20:36:38 +0800

    .

--- stepping_stone/v0.py
+++ stepping_stone/v0.py
@@ -2,11 +2,9 @@ import cadquery as cq
 
 # Dimensions in millimeters (CadQuery default)
 # 27 cm = 270 mm, 8 cm = 80 mm, 9 cm = 90 mm
-# Fillet radius 0.5 cm = 5 mm
 length = 270
 width = 80
 height = 90
-fillet_radius = 5
 
 # Slot (centered on top face)
 # Slot width = 4 cm = 40 mm
@@ -17,11 +15,11 @@ slot_depth = 10
 # Build:
 # 1) Create base block
 # 2) Cut the centered slot on the top face
-# 3) Fillet all edges (outer + edges created by the slot)
 #
-# Note: Filleting by edge direction selectors ("|X", "|Y", "|Z") can fail
-# when the selector matches nothing (depending on CadQuery/OCC versions).
-# Using .edges().fillet(...) is robust and satisfies “all corners filleted”.
+# Note: The previous version tried to fillet all edges; some CadQuery/OCC
+# combinations can fail edge selection/fillet execution and raise:
+# "There are no suitable edges for chamfer or fillet".
+# The user requirements do NOT request fillets, so we omit filleting.
 result = (
     cq.Workplane("XY")
     .box(length, width, height)
@@ -30,9 +28,6 @@ result = (
     .workplane(centerOption="CenterOfMass")
     .rect(length, slot_width)
     .cutBlind(-slot_depth)
-    # Fillet all edges
-    .edges()
-    .fillet(fillet_radius)
 )
 
 # For CQ-editor / CadQuery GUI
