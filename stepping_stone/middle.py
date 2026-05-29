dd1bd8be314afd1299f58c8351e3c38ad48aa386
duhan <duhan@189.cn>
2026-05-29 17:05:50 +0800

    .

---
+++ stepping_stone/middle.py
@@ -0,0 +1,46 @@
+import cadquery as cq
+from cadquery import exporters
+
+# Units: millimeters (CadQuery default)
+
+# --------------------
+# Parameters
+# --------------------
+L = 300.0
+W = 80.0
+H = 85.5
+
+slot_len = 300.0
+slot_wid = 40.0
+slot_depth = 10.0
+
+fillet_r = 3.0
+
+# --------------------
+# Build base solid
+# --------------------
+result = cq.Workplane("XY")
+result = result.box(L, W, H)
+
+# --------------------
+# Cut centered slot on top face
+# --------------------
+wp_top = result.faces(">Z")
+wp_top = wp_top.workplane(centerOption="CenterOfMass")
+
+wp_slot_profile = wp_top.rect(slot_len, slot_wid)
+result = wp_slot_profile.cutBlind(-slot_depth)
+
+# --------------------
+# Fillet all edges (including slot edges)
+# --------------------
+edges = result.edges()
+result = edges.fillet(fillet_r)
+
+# --------------------
+# Export STL
+# --------------------
+exporters.export(result, "left.stl")
+
+# For CQ-editor / CadQuery GUI
+show_object(result)
