febd85a43f892db58024a966b939cb6f7115eb77
duhan <duhan@189.cn>
2026-05-29 17:16:36 +0800

    .

--- stepping_stone/right.py
+++ stepping_stone/right.py
@@ -10,10 +10,6 @@ L = 300.0
 W = 80.0
 H = 75.5
 
-slot_len = 300.0
-slot_wid = 40.0
-slot_depth = 10.0
-
 fillet_r = 3.0
 
 # --------------------
@@ -23,16 +19,7 @@ result = cq.Workplane("XY")
 result = result.box(L, W, H)
 
 # --------------------
-# Cut centered slot on top face
-# --------------------
-wp_top = result.faces(">Z")
-wp_top = wp_top.workplane(centerOption="CenterOfMass")
-
-wp_slot_profile = wp_top.rect(slot_len, slot_wid)
-result = wp_slot_profile.cutBlind(-slot_depth)
-
-# --------------------
-# Fillet all edges (including slot edges)
+# Fillet all edges
 # --------------------
 edges = result.edges()
 result = edges.fillet(fillet_r)
