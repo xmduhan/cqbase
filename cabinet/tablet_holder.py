e6e10e24cab0ef37702ec6be1f16bde652a41408
duhan <duhan@189.cn>
2026-06-18 20:26:55 +0800

    .

--- cabinet/tablet_holder.py
+++ cabinet/tablet_holder.py
@@ -1,16 +1,17 @@
 import cadquery as cq
 from cadquery import exporters
 
-L = 315
-W = 300
-H = 200
+L = 319
+W = 275
+H = 195
 
-slot_wid = 25
+slot_wid = 32
 slot_border = 9
+slot_start = 0
 
 fillet_r = 3
-cut_y = 120   
-cut_z = H   
+cut_y = 40
+cut_z = H
 
 ymin, ymax = -W/2, W/2
 zmin, zmax = -H/2, H/2
@@ -19,16 +20,17 @@ tri_lb = [(ymin, zmin), (ymin + cut_y, zmin), (ymin, zmin + cut_z)]
 tri_rt = [(ymax, zmax), (ymax - cut_y, zmax), (ymax, zmax - cut_z)]
 
 rect = [
-    (ymin , zmax), 
-    (ymin + slot_wid, zmax), 
-    (ymin + cut_y + slot_wid, zmin),
-    (ymin + cut_y, zmin), 
+    (ymin + slot_start, zmax),
+    (ymin + slot_start + slot_wid, zmax),
+    (ymin + slot_start + cut_y + slot_wid, zmin),
+    (ymin + slot_start + cut_y, zmin),
 ]
 
 result = (
     cq.Workplane("XY")
     .box(L, W, H)
 )
+
 
 result = (
     result.faces(">X")
@@ -44,66 +46,44 @@ result = (
     .cutThruAll()
 )
 
-# rects = []
-# for i in range(5):
-#     rects.append(
-#         result.faces(">X")
-#         .center(slot_border * (i + 1) + slot_wid * i, 0)
-#         .polyline(rect).close()
-#     )
-# result.add(rects)
-# result = result.cutThruAll()
+###################################################
+workplane = result.faces(">X").workplane()
 
-# result = rects[0].cutThruAll()
-
-
-for i in range(5):
-    result.add(
+for i in range(6):
+    workplane.add(
         result.faces(">X")
         .center(slot_border * (i + 1) + slot_wid * i, 0)
         .polyline(rect).close()
     )
-    result = result.cutThruAll()
-# result = result.cutThruAll()
-# result = result.cutThruAll()
 
-# result = result.cutThruAll()
+result = workplane.cutThruAll()
 
 
-# result = (
-#     result.faces(">X")
-#     .center([(slot_border * (i + 1) + slot_wid * i, 0) for i in range(5)])
-#     .polyline(rect).close()
-# )
+result = (
+    result.faces(">X")
+    .workplane(centerOption="CenterOfBoundBox")
+    .box(W, H, slot_border - 2)
+)
 
-# result = (
-#     result.faces(">X")
-#     .workplane(centerOption="CenterOfBoundBox")
-#     .box(W, H, slot_border)
-# )
+result = (
+    result.faces("<X")
+    .workplane(centerOption="CenterOfBoundBox")
+    .box(W, H, slot_border - 2)
+)
 
-# result = (
-#     result.faces("<X")
-#     .workplane(centerOption="CenterOfBoundBox")
-#     .box(W, H, slot_border)
-# )
-
-# result = (
-#     result.faces("Y")
-#     .workplane(centerOption="CenterOfBoundBox")
-#     .box(L + slot_border, H , slot_border)
-# )
-
-# result = (
-#     result.faces("<Z")
-#     .workplane(centerOption="CenterOfBoundBox")
-#     .box(L + slot_border, W + slot_border / 2, slot_border)
-# )
+result = (
+    result.faces("<Z")
+    .workplane(centerOption="CenterOfBoundBox")
+    .box(L + slot_border - 2, W , slot_border)
+)
 
 
-# result = result.faces(">Z").edges().fillet(fillet_r)
+result = result.faces(">Z").edges().fillet(fillet_r)
+result = result.faces("<Z").edges().fillet(fillet_r)
+result = result.faces(">Y").edges().fillet(2)
+result = result.faces("<Y").edges().fillet(2)
 
 
-# show_object(result)
+show_object(result)
 
 
